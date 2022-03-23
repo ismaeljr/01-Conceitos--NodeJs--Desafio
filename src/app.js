fbbc529840815c211fec6f4f62916be4f7eeeb59
Skywalker <ismael_junior@msn.com>
2022-03-23 17:32:47 -0300

    Primeiro desafio GoStack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,86 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  // escrever codigo para listar repositorios
+
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  // escrever codigo para cadastrar repositorio
+  const {title, url, techs} = request.body;
+  
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  // escrever codigo que altere o repositorio
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  
+  //devemos pegar o indice
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({error: "Id not Found!"});
+  }
+
+  const repositorie = {
+    id: repositories[repositorieIndex].id,
+    title,
+    url,
+    techs,
+    likes :repositories[repositorieIndex].likes
+  }
+  
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  // escrever codigo para deletar
+  const { id } = request.params;
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({error: "Id not Found!"})
+  }
+
+  repositories.splice(repositorieIndex,1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  // escrever codigo para aumentar o like enm +1
+  const { id } = request.params;
+  //onst {title, url, techs, likes} = request.body;
+    
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return response.status(400).json({error: "Id not Found!"})
+  }
+  repositories[repositorieIndex].likes++;
+  
+  return response.json(repositories[repositorieIndex]);
+
 });
 
 module.exports = app;
